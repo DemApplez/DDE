31ba5fb7e5dcd815ef9de3f3e5e5091bf6ad567d
Dem Apples <50384803+DemApplez@users.noreply.github.com>
2023-01-28 01:13:08 -0500

    Test, please ignore
    shut up

---
+++ startupLogo.lua
@@ -0,0 +1,28 @@
+term.clear()
+term.setCursorPos(1,1)
+
+print("Welcome To")
+
+print("")
+
+print("::::    ::::  ::::::::::: ::::::::   ::::::::  ")
+sleep(0.1)
+print("+:+:+: :+:+:+     :+:    :+:    :+: :+:    :+: ")
+sleep(0.1)
+print("+:+ +:+:+ +:+     +:+    +:+    +:+ +:+        ")
+sleep(0.1)
+print("+#+  +:+  +#+     +#+    +#+    +:+ +#++:++#++ ")
+sleep(0.1)
+print("+#+       +#+     +#+    +#+    +#+        +#+ ")
+sleep(0.1)
+print("#+#       #+#     #+#    #+#    #+# #+#    #+# ")
+sleep(0.1)
+print("###       ### ########### ########   ######## ")
+
+print("")
+
+print("Main Dev Build#0295")
+
+sleep(2)
+
+shell.run("mIOS/.start")
